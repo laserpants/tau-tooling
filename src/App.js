73f37917d2bc2a6a938b3cf2042a3d0455e5bf93
Heikki Johannes Hilden <hildenjohannes@gmail.com>
2021-06-26 14:18:40 +0300

    Add workaround to remove accessibility borders

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import {
   Box,
   ChakraProvider,
 } from "@chakra-ui/react";
+import { Global, css } from "@emotion/react";
 
 function PaneSection({ title, children }) {
   return (
@@ -81,9 +82,21 @@ function Layout() {
   );
 }
 
+const GlobalStyles = css`
+  /*
+    This will hide the focus indicator if the element receives focus via the mouse,
+    but it will still show up on keyboard focus.
+  */
+  .js-focus-visible :focus:not([data-focus-visible-added]) {
+    outline: none;
+    box-shadow: none;
+  }
+`;
+
 function App() {
   return (
     <ChakraProvider>
+      <Global styles={GlobalStyles} />
       <Box w="100%" h="100vh">
         <Layout />
       </Box>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import "focus-visible/dist/focus-visible";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
