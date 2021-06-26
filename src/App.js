fb8194fcbdc9a3b60589f13f6a5f08a5bcb147bc
Heikki Johannes Hilden <hildenjohannes@gmail.com>
2021-06-26 15:44:01 +0300

    Build out UI

--- src/App.js
+++ src/App.js
@@ -9,6 +9,15 @@ import {
   AccordionPanel,
   VStack,
   Box,
+  Link,
+  Table,
+  Text,
+  Th,
+  Tr,
+  Td,
+  Tbody,
+  Tfoot,
+  Thead,
   ChakraProvider,
 } from "@chakra-ui/react";
 import { Global, css } from "@emotion/react";
@@ -16,25 +25,42 @@ import { Global, css } from "@emotion/react";
 function PaneSection({ title, children }) {
   return (
     <AccordionItem>
-      <h2>
-        <AccordionButton className="no-select">
+      <h2 style={{ background: "rgba(0, 0, 0, 0.08)" }}>
+        <AccordionButton className="no-select" px={2}>
+          <AccordionIcon mr={1} />
           <Box flex="1" textAlign="left">
             {title}
           </Box>
-          <AccordionIcon />
         </AccordionButton>
       </h2>
-      <AccordionPanel pb={4}>{children}</AccordionPanel>
+      <AccordionPanel py={0} px={0}>
+        {children}
+      </AccordionPanel>
     </AccordionItem>
   );
 }
 
 function ModuleInfo() {
+  const TableRow = ({ first, second }) => (
+    <Tr cursor="pointer" _hover={{ background: "rgba(0, 0, 0, 0.08)" }}>
+      <Td py={1}>{first}</Td>
+      <Td py={1}>{second}</Td>
+    </Tr>
+  );
+
   return (
     <>
       <Accordion reduceMotion={true} defaultIndex={[0]} allowMultiple>
         <PaneSection title="Imports">section 1</PaneSection>
-        <PaneSection title="Definitions">section 2</PaneSection>
+        <PaneSection title="Definitions">
+          <Table size="sm">
+            <Tbody>
+              <TableRow first="main" second="Int &rarr; IO ()" />
+              <TableRow first="factorial" second="Nat &rarr; Nat" />
+              <TableRow first="toString" second="a &rarr; String" />
+            </Tbody>
+          </Table>
+        </PaneSection>
         <PaneSection title="Type declarations">section 3</PaneSection>
         <PaneSection title="Classes">section 4</PaneSection>
         <PaneSection title="Class instances">section 5</PaneSection>
