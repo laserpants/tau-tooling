4d181ea2e85ba7460f4f6cf1bf8403d31161fe30
Heikki Johannes Hilden <hildenjohannes@gmail.com>
2021-06-26 13:26:24 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
+import React from "react";
+import SplitPane from "react-split-pane";
+import { Box, ChakraProvider } from "@chakra-ui/react";
+
+function Layout() {
+  return (
+    <>
+      <SplitPane split="horizontal" defaultSize={100}>
+        <div>xx</div>
+        <SplitPane split="horizontal" defaultSize={200} primary="second">
+          <SplitPane split="vertical" defaultSize={150}>
+            <div>pane1</div>
+            <SplitPane split="vertical" defaultSize={200}>
+              <div>pane2</div>
+              <div>pane3</div>
+            </SplitPane>
+          </SplitPane>
+          <div>xx</div>
+        </SplitPane>
+      </SplitPane>
+    </>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChakraProvider>
+      <Box w="100%" h="100vh">
+        <Layout />
+      </Box>
+    </ChakraProvider>
   );
 }
 
