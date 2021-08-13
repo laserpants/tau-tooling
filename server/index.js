15af0d38959708ce28be209e92317d292999d214
laserpants <hildenjohannes@gmail.com>
2021-08-13 20:14:02 +0300

    Refactor

--- server/index.js
+++ server/index.js
@@ -5,12 +5,11 @@ const { exec } = require('child_process');
 
 const app = express();
 const port = 3102;
-const cmd = '/home/laserpants/code/tau/sandbox5/.stack-work/dist/x86_64-linux-tinfo6/Cabal-3.2.1.0/build/tau-exe/tau-exe';
+const cmd = '/home/laserpants/code/tau/sandbox6/.stack-work/dist/x86_64-linux-tinfo6/Cabal-3.2.1.0/build/tau-exe/tau-exe';
 
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
-
 
 app.post('/run', (req, res) => {
   exec(`${cmd} "${req.body.source}"`, (error, stdout, stderr) => {
